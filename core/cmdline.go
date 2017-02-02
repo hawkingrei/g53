c1413595e3c4731de3c6141be1aff4fe4a327c32
真人 <weizhen.wang@duitang.com>
2017-02-02 15:33:16 +0800

    feature: remove tls support temporarily

--- core/cmdline.go
+++ core/cmdline.go
@@ -30,11 +30,6 @@ func (cmdline *CommandLine) ParseParameters(rawParams []string) (res *utils.Conf
 	http := app.Flag("http", "Listen HTTP requests on this address").Default(res.HttpAddr).Short('t').String()
 	domain := app.Flag("domain", "Domain that is appended to all requests").Default(res.Domain.String()).String()
 	environment := app.Flag("environment", "Optional context before domain suffix").Default("").String()
-	//docker := app.Flag("docker", "Path to the docker socket").Default(res.DockerHost).String()
-	tlsverify := app.Flag("tlsverify", "Enable mTLS when connecting to docker").Default(strconv.FormatBool(res.TlsVerify)).Bool()
-	tlscacert := app.Flag("tlscacert", "Path to CA certificate").Default(res.TlsCaCert).String()
-	tlscert := app.Flag("tlscert", "Path to Client certificate").Default(res.TlsCert).String()
-	tlskey := app.Flag("tlskey", "Path to client certificate private key").Default(res.TlsKey).String()
 	ttl := app.Flag("ttl", "TTL for matched requests").Default(strconv.FormatInt(int64(res.Ttl), 10)).Int()
 	createAlias := app.Flag("alias", "Automatically create an alias with just the container name.").Default(strconv.FormatBool(res.CreateAlias)).Bool()
 	verbose := app.Flag("verbose", "Verbose mode.").Default(strconv.FormatBool(res.Verbose)).Short('v').Bool()
@@ -48,11 +43,6 @@ func (cmdline *CommandLine) ParseParameters(rawParams []string) (res *utils.Conf
 	res.DnsAddr = *dns
 	res.HttpAddr = *http
 	res.Domain = utils.NewDomain(fmt.Sprintf("%s.%s", *environment, *domain))
-	//res.DockerHost = *docker
-	res.TlsVerify = *tlsverify
-	res.TlsCaCert = *tlscacert
-	res.TlsCert = *tlscert
-	res.TlsKey = *tlskey
 	res.Ttl = *ttl
 	res.CreateAlias = *createAlias
 	return
