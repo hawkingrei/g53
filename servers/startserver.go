205b1f447d0bdf456761f84f1ca11266dc7b3be8
真人 <weizhen.wang@duitang.com>
2017-02-06 18:57:24 +0800

    style: remove tls

--- servers/startserver.go
+++ servers/startserver.go
@@ -1,10 +1,6 @@
 package servers
 
 import (
-	"crypto/tls"
-	"crypto/x509"
-	"io/ioutil"
-
 	"github.com/op/go-logging"
 
 	"github.com/hawkingrei/G53/core"
@@ -32,26 +28,6 @@ func StartServer(rawParams []string) {
 	}
 
 	dnsServer := NewDNSServer(config)
-
-	var tlsConfig *tls.Config
-	if config.TlsVerify {
-		clientCert, err := tls.LoadX509KeyPair(config.TlsCert, config.TlsKey)
-		if err != nil {
-			logger.Fatalf("Error: '%s'", err)
-		}
-		tlsConfig = &tls.Config{
-			MinVersion:   tls.VersionTLS12,
-			Certificates: []tls.Certificate{clientCert},
-		}
-		pemData, err := ioutil.ReadFile(config.TlsCaCert)
-		if err == nil {
-			rootCert := x509.NewCertPool()
-			rootCert.AppendCertsFromPEM(pemData)
-			tlsConfig.RootCAs = rootCert
-		} else {
-			logger.Fatalf("Error: '%s'", err)
-		}
-	}
 	httpServer := NewHTTPServer(config, dnsServer)
 	go func() {
 		if err := httpServer.Start(); err != nil {
