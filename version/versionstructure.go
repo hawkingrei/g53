df99facc60ee6d31e6d37a56021c87942bed3475
真人 <weizhen.wang@duitang.com>
2017-02-14 12:23:28 +0800

    update

---
+++ version/versionstructure.go
@@ -0,0 +1,10 @@
+package version
+
+type VersionOptions struct {
+        GitCommit string
+        Version   string
+        BuildTime string
+        GoVersion string
+        Os        string
+        Arch      string
+}
